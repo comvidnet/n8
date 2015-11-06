4a9a956cab49e2c5e8f06da453d875ae964ed0fc
David Clarisse <david@ilumy.com>
2015-11-06 09:46:06 +0100

    Initial version

---
+++ www/js/app.js
@@ -0,0 +1,46 @@
+angular.module('app', ['ionic','ionic.service.core', 'ngLodash', 'app.controllers', 'app.directives'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    Ionic.io();
+
+    var push = new Ionic.Push({
+      "debug": true
+    });
+
+    push.register(function(token) {
+      console.log("Device token:",token.token);
+    });
+
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.config(['$stateProvider', '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home')
+
+    $stateProvider
+      //.state('app', {
+      //  url: "/app",
+      //  abstract: true,
+      //  templateUrl: "js/templates/menu.html",
+      //  controller: 'AppCtrl'
+      //})
+      .state('index', {
+        url: '/home',
+        templateUrl: 'js/templates/index.html',
+        controller: 'IndexCtrl'
+      });
+    $stateProvider.state('map', {
+        url: '/map',
+        templateUrl: 'js/templates/map.html',
+        controller: 'MapCtrl'
+      });
+    $stateProvider.state('mapStep', {
+      url: '/mapStep',
+      templateUrl: 'js/templates/map-step.html',
+      controller: 'MapStepCtrl'
+    });
+  }])
---
+++ www/js/controllers.js
@@ -0,0 +1,182 @@
+var toplocations = []
+toplocations.push({
+  location: 'Rijksmuseum Amsterdam',
+  latitude: 1,
+  longitude: 2,
+  time: 120
+});
+
+var rp = [];
+rp.push({
+  location: 'Paleis dam Amsterdam',
+  type: 'building',
+  time: 2,
+  latitude: 1,
+  longitude: 2,
+  next: 'Hermitage Amsterdam'
+});
+
+rp.push({
+  location: 'Hermitage Amsterdam',
+  type: 'museum',
+  time: 90,
+  next: 'Artis Amsterdam'
+});
+
+rp.push({
+  location: 'Artis Amsterdam',
+  type: 'food',
+  time: 60,
+  next: 'Tropenmuseum Amsterdam'
+});
+rp.push({
+  location: 'Tropenmuseum Amsterdam',
+  type: 'museum',
+  time: 75,
+  travel: 'public',
+  next: 'Rijksmuseum'
+});
+
+angular.module('app.controllers', [])
+  .controller('MainCtrl', function($scope, $ionicLoading) {
+
+  })
+  .controller('IndexCtrl', ['$scope', '$state', function($scope, $state) {
+    $scope.goToMap = function () {
+      $state.go('app.map');
+    };
+  }])
+.controller('MapCtrl', function($scope, $ionicLoading, lodash) {
+    $scope.routeInfo = [];
+    $scope.loading = $ionicLoading.show({
+      content: 'Loading...',
+      showBackdrop: false
+    });
+
+    $scope.mapCreated = function(map) {
+    $scope.map = map;
+
+      var waypts = lodash.map(rp, function(wp) {
+        return {
+          location: wp.location
+        }
+      })
+
+      var directionsService = new google.maps.DirectionsService;
+      var directionsDisplay = new google.maps.DirectionsRenderer(/*{suppressMarkers: true}*/);
+      directionsDisplay.setMap(map);
+      navigator.geolocation.getCurrentPosition(function (pos) {
+        directionsService.route({
+          origin: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+          destination: toplocations[0].location,
+          waypoints: waypts,
+          optimizeWaypoints: true,
+          travelMode: google.maps.TravelMode.WALKING
+        }, function (response, status) {
+          if (status === google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+            $scope.loading.hide();
+
+            var route = response.routes[0];
+            //var summaryPanel = document.getElementById('directions-panel');
+            //summaryPanel.innerHTML = '';
+            // For each route, display summary information.
+
+            $scope.$apply(function () {
+              for (var i = 0; i < route.legs.length; i++) {
+                $scope.routeInfo.push({
+                  text: route.legs[i].start_address,
+                  duration: route.legs[i].duration.text});
+                //  var routeSegment = i + 1;
+                //  summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                //    '</b><br>';
+                //  summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+                //  summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+                //  summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+              }
+            });
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      });
+
+    var myloc = new google.maps.Marker({
+      clickable: false,
+      icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+        new google.maps.Size(22,22),
+        new google.maps.Point(0,18),
+        new google.maps.Point(11,11)),
+      shadow: null,
+      zIndex: 999,
+      map: $scope.map// your google.maps.Map object
+    });
+
+    var watchID = navigator.geolocation.watchPosition(function (pos) {
+      console.log('Polling pos', pos);
+      //$scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+
+      var me = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+      myloc.setPosition(me);
+    }, function (error) {
+      alert('Unable to get location: ' + error.message);
+    }, { enableHighAccuracy: true });
+  };
+
+  $scope.centerOnMe = function () {
+    console.log("Centering");
+    if (!$scope.map) {
+      return;
+    }
+
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
+
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      console.log('Got pos', pos);
+      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      $scope.loading.hide();
+    }, function (error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
+
+
+
+})
+  .controller('MapStepCtrl', function($scope, $ionicLoading, lodash) {
+
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer(/*{suppressMarkers: true}*/);
+
+    $scope.mapCreated = function(map) {
+      $scope.map = map;
+
+      directionsDisplay.setMap(map);
+      $scope.step = 0;
+      $scope.nextStep();
+
+    }
+
+
+    $scope.nextStep = function() {
+        var loc =rp[ $scope.step];
+        var ds = {
+          origin: loc.location,
+          destination: loc.next,
+        }
+        if (loc.travel == 'public')
+          ds.travelMode = google.maps.TravelMode.TRANSIT;
+        else
+          ds.travelMode = google.maps.TravelMode.WALKING;
+        directionsService.route(ds, function (response, status) {
+          if (status === google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response)
+          }
+        });
+      $scope.step++;
+    };
+
+  });
